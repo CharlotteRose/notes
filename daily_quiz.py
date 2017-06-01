84c144a31cfc51887f92b34c4ccc2a13ad49b74f
Charlotte Rose Hamilton <charlottesemail@icloud.com>
2017-06-01 08:34:45 -0400

    list additions

--- daily_quiz.py
+++ daily_quiz.py
@@ -23,6 +23,16 @@ Htppd_logs=['/var/log/httpd/access_log' ,'/var/log/httpd/error_log']
 print (Htppd_logs.__len__())
 #/etc/sysconfg/<service_mname>  |  main config files for servies
 #systemd.journal-fields
+answer = input("If unable to use a web browser which command line tool\n"
+               "would you use to view the contents of a web page?")
+answer = answer.lower() # optimized way to have input lowered upon entry?
+print (answer)
+#one option is use reference aprops to see similar commands
 
 #file_permissions
 #per
+#SHOULD BE ABLE TO VIEW FILE OF MAN PAGE -> groff typesetting is used -> .SH *should* denote header or section header
+
+#Commands to include
+#ausearch
+#curl, elinks
